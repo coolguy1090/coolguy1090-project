c624480a2f6a1c03bd2b164e995875b5eced0e72
coolguy1090 <kennardelijah@icloud.com>
2024-08-31 14:48:12 -0400

    Update coolguy.lua

--- coolguy.lua
+++ coolguy.lua
@@ -429,6 +429,7 @@ addcmd("vgc", -- similar to kohlslite but i swear i didnt skid it.
 function()
     chat("gear me 00000000000000094794847")
     local tool = lp.Backpack:FindFirstChild("VampireVanquisher")
+     wait(0.3)
 tool.Parent = game.Players.LocalPlayer.Character
 tool:Activate()
 wait(0.3)

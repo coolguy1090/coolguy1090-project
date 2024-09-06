1fa611be447dd9ea35d99918b64a47a26451ff1f
coolguy1090 <kennardelijah@icloud.com>
2024-09-05 20:07:21 -0400

    Update Command Handler.lua

--- Command Handler.lua
+++ Command Handler.lua
@@ -1,10 +1,12 @@
 local commands = {}
 local descriptions = {}
 local connections = {}
+
+--- Edited ScV2 Handler. ---
 function addcommand(information)
 	local cmdName = information.name
         local cmdDescription = information.desc
-	local cmdFunction = information.func
+	local cmdFunction = information.Function
             commands[cmdName] = cmdName
             descriptions[cmdDescription] = cmdDescription
             connections[#connections + 1] = game.Players.LocalPlayer.Chatted:Connectu(function(msg)
@@ -16,9 +18,9 @@ end)
 end
 
 addcommand({
-        name = "cool",
-        desc = "a",
-            func = Function()
+        Name = "cool",
+        Desc = "a",
+         Function = Function()
 game.Players:Chat("h a")
 end
 })

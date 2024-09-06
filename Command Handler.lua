12bea4cb33201115be0f79a96839e4975142b387
coolguy1090 <kennardelijah@icloud.com>
2024-09-05 20:02:59 -0400

    Update Command Handler.lua

--- Command Handler.lua
+++ Command Handler.lua
@@ -1,7 +1,13 @@
+
+local commands = {}
+local descriptions = {}
+local connections = {}
 function addcommand(information)
 	local cmdName = information.name
         local cmdDescription = information.desc
 	local cmdFunction = information.func
+            commands[cmdName] = cmdName
+            descriptions[cmdDescription] = cmdDescription
             connections[#connections + 1] = game.Players.LocalPlayer.Chatted:Connectu(function(msg)
                     local args = msg:split(" ")
             if args[1] == prefix .. cmdName then

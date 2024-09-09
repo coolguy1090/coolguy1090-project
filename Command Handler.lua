f6e6f08a9d82d31e056c7a9a755677d151ddc62d
coolguy1090 <kennardelijah@icloud.com>
2024-09-08 21:01:46 -0400

    Update Command Handler.lua

--- Command Handler.lua
+++ Command Handler.lua
@@ -197,7 +197,7 @@ addcommand({ --- test
 
 							task.spawn(function()
         if not connections.bang2 then
-				          connections.bang2 = game:GetService("RunService").RenderStepped:Wait()
+				          connections.bang2 = rs.RenderStepped:Wait()
         for i, gear in pairs(bgears) do
                         for i,v in pairs(game.Players:GetPlayers()) do
             if v.Name ~= lp.Name and not table.find(gwl, v.Name) then

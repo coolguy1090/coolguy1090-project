b1e208239a96c0ec703f1506b3889ca03b916e02
coolguy1090 <kennardelijah@icloud.com>
2024-09-16 20:48:39 -0400

    Update string.lua

--- string.lua
+++ string.lua
@@ -1,5 +1,10 @@
+local connections = {}
+
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
 	
 				if string.sub(msg, 0 #msg) == "a" then
 end
+
+						if string.sub(msg, 0, #msg) == "aa" then
+							end
 end)
